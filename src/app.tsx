2e18a68d976a050cd94f937e9b20aacba96a383a
Bruno Petrolini <brunopetrolini@hotmail.com>
2024-07-16 13:30:33 -0300

    refactor: runs lint

--- src/app.tsx
+++ src/app.tsx
@@ -16,14 +16,13 @@ export function App() {
           <p className="text-zinc-300 text-lg">Convide seus amigos e planeje sua próxima viagem!</p>
         </div>
 
-        <div className='space-y-4'>
-
+        <div className="space-y-4">
           {/* Destination Input */}
           <div className="h-16 bg-zinc-900 px-4 rounded-xl flex items-center shadow-shape gap-3">
             <div className="flex items-center gap-2 flex-1">
               <MapPin className="size-5 text-zinc-400" />
               <input
-                disabled={isGuestInputOpen} 
+                disabled={isGuestInputOpen}
                 type="text"
                 placeholder="Para onde você vai?"
                 className="bg-transparent text-lg placeholder-zinc-400 outline-none flex-1"
@@ -33,7 +32,7 @@ export function App() {
             <div className="flex items-center gap-2">
               <Calendar className="size-5 text-zinc-400" />
               <input
-                disabled={isGuestInputOpen} 
+                disabled={isGuestInputOpen}
                 type="text"
                 placeholder="Quando?"
                 className="bg-transparent text-lg placeholder-zinc-400 w-40 outline-none"
@@ -43,46 +42,46 @@ export function App() {
             <div className="w-px h-6 bg-zinc-800" />
 
             {isGuestInputOpen ? (
-              <button 
-                className='bg-zinc-800 text-zinc-200 rounded-lg px-5 py-2 font-medium flex items-center gap-2 hover:bg-zinc-700'
-                onClick={toggleGuestInput}  
+              <button
+                className="bg-zinc-800 text-zinc-200 rounded-lg px-5 py-2 font-medium flex items-center gap-2 hover:bg-zinc-700"
+                onClick={toggleGuestInput}
               >
                 Alterar local/data
                 <Settings2 className="size-5" />
               </button>
             ) : (
-              <button 
-              className="bg-lime-300 text-lime-950 rounded-lg px-5 py-2 font-medium flex items-center gap-2 hover:bg-lime-400" 
-              onClick={toggleGuestInput}
-            >
-              Continuar
-              <ArrowRight className="size-5" />
-            </button>
+              <button
+                className="bg-lime-300 text-lime-950 rounded-lg px-5 py-2 font-medium flex items-center gap-2 hover:bg-lime-400"
+                onClick={toggleGuestInput}
+              >
+                Continuar
+                <ArrowRight className="size-5" />
+              </button>
             )}
           </div>
 
-          {/* Guest Input */}        
+          {/* Guest Input */}
           {isGuestInputOpen && (
             <div className="h-16 bg-zinc-900 px-4 rounded-xl flex items-center shadow-shape gap-3">
-            <div className="flex items-center gap-2 flex-1">
-              <UserRoundPlus className="size-5 text-zinc-400" />
-              <input
-                type="text"
-                placeholder="Quem estará na viagem?"
-                className="bg-transparent text-lg placeholder-zinc-400 outline-none flex-1"
-              />
+              <div className="flex items-center gap-2 flex-1">
+                <UserRoundPlus className="size-5 text-zinc-400" />
+                <input
+                  type="text"
+                  placeholder="Quem estará na viagem?"
+                  className="bg-transparent text-lg placeholder-zinc-400 outline-none flex-1"
+                />
+              </div>
+
+              <div className="w-px h-6 bg-zinc-800" />
+
+              <button
+                className="bg-lime-300 text-lime-950 rounded-lg px-5 py-2 font-medium flex items-center gap-2 hover:bg-lime-400"
+                onClick={() => alert('Viagem confirmada!')}
+              >
+                Confirmar viagem
+                <ArrowRight className="size-5" />
+              </button>
             </div>
-
-            <div className="w-px h-6 bg-zinc-800" />
-
-            <button 
-              className="bg-lime-300 text-lime-950 rounded-lg px-5 py-2 font-medium flex items-center gap-2 hover:bg-lime-400" 
-              onClick={() => alert('Viagem confirmada!')}
-            >
-              Confirmar viagem
-              <ArrowRight className="size-5" />
-            </button>
-          </div>
           )}
         </div>
 

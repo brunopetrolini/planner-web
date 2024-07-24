229c52c8af54887e03450e8f37d554c162d93b33
Bruno Petrolini <brunopetrolini@hotmail.com>
2024-07-24 16:38:55 -0300

    feat: render activities list

--- src/pages/trip-details/components/activities-list.tsx
+++ src/pages/trip-details/components/activities-list.tsx
@@ -1,11 +1,56 @@
-import { LucideCircleCheck, Plus } from 'lucide-react';
+import { constructNow, format, isAfter } from 'date-fns';
+import { ptBR } from 'date-fns/locale';
+import { CircleDashed, LucideCircleCheck, Plus } from 'lucide-react';
+import { useEffect, useState } from 'react';
+import { useParams } from 'react-router-dom';
 import { Button } from '../../../components/button';
+import { api } from '../../../lib/axios';
 
 interface ActivitiesListProps {
   toggleCreateActivityModal: () => void;
 }
 
+interface Activity {
+  date: string;
+  activities: {
+    id: string;
+    title: string;
+    occurs_at: string;
+    trip_id: string;
+  }[];
+}
+
 export function ActivitiesList({ toggleCreateActivityModal }: ActivitiesListProps) {
+  const { tripId } = useParams();
+
+  const [activities, setActivities] = useState<Activity[]>([]);
+
+  useEffect(() => {
+    api.get(`/trips/${tripId}/activities`).then((response) => setActivities(response.data.activities));
+  }, [tripId]);
+
+  function renderDayItem(activities: Activity['activities']) {
+    return activities.map((activity) => {
+      const occursAt = format(activity.occurs_at, 'HH:mm', { locale: ptBR });
+
+      const hasHappen = isAfter(constructNow(new Date()), activity.occurs_at);
+
+      return (
+        <div key={activity.id} className="space-y-2.5">
+          <div className="px-4 py-2.5 bg-zinc-900 rounded-xl shadow-shape flex items-center gap-3">
+            {hasHappen ? (
+              <LucideCircleCheck className="size-5 text-lime-300" />
+            ) : (
+              <CircleDashed className="size-5 text-zinc-400" />
+            )}
+            <span className="text-zinc-100">{activity.title}</span>
+            <span className="text-zinc-400 text-sm ml-auto">{`${occursAt}h`}</span>
+          </div>
+        </div>
+      );
+    });
+  }
+
   return (
     <div className="flex-1 space-y-6">
       {/* Header */}
@@ -21,38 +66,25 @@ export function ActivitiesList({ toggleCreateActivityModal }: ActivitiesListProp
       {/* Activities */}
       <div className="space-y-8">
         {/* Activity Day */}
-        <div className="space-y-2.5">
-          <div className="flex gap-2 items-baseline">
-            <span className="text-xl text-zinc-300 font-semibold">Dia 17</span>
-            <span className="text-xs text-zinc-500">Sábado</span>
-          </div>
+        {activities.map((activity) => {
+          const activityDay = format(activity.date, 'd', { locale: ptBR });
+          const activityExtendedDay = format(activity.date, 'EEEE', { locale: ptBR });
 
-          <p className="text-zinc-500 text-sm">Nenhuma atividade cadastrada nessa data.</p>
-        </div>
+          return (
+            <div key={activity.date} className="space-y-2.5">
+              <div className="flex gap-2 items-baseline">
+                <span className="text-xl text-zinc-300 font-semibold">{`Dia ${activityDay}`}</span>
+                <span className="text-xs text-zinc-500">{activityExtendedDay}</span>
+              </div>
 
-        {/* Activity Day */}
-        <div className="space-y-2.5">
-          <div className="flex gap-2 items-baseline">
-            <span className="text-xl text-zinc-300 font-semibold">Dia 18</span>
-            <span className="text-xs text-zinc-500">Domingo</span>
-          </div>
-
-          <div className="space-y-2.5">
-            <div className="px-4 py-2.5 bg-zinc-900 rounded-xl shadow-shape flex items-center gap-3">
-              <LucideCircleCheck className="size-5 text-lime-300" />
-              <span className="text-zinc-100">Academia em grupo</span>
-              <span className="text-zinc-400 text-sm ml-auto">08:00h</span>
+              {activity.activities.length > 0 ? (
+                renderDayItem(activity.activities)
+              ) : (
+                <p className="text-zinc-500 text-sm">Nenhuma atividade cadastrada nessa data.</p>
+              )}
             </div>
-          </div>
-
-          <div className="space-y-2.5">
-            <div className="px-4 py-2.5 bg-zinc-900 rounded-xl shadow-shape flex items-center gap-3">
-              <LucideCircleCheck className="size-5 text-lime-300" />
-              <span className="text-zinc-100">Almoço</span>
-              <span className="text-zinc-400 text-sm ml-auto">12:00h</span>
-            </div>
-          </div>
-        </div>
+          );
+        })}
       </div>
     </div>
   );
--- src/pages/trip-details/modals/create-activity-modal.tsx
+++ src/pages/trip-details/modals/create-activity-modal.tsx
@@ -1,12 +1,28 @@
 import { Calendar, Tag, X } from 'lucide-react';
+import { useParams } from 'react-router-dom';
 import { Button } from '../../../components/button';
 import { Input } from '../../../components/input';
+import { api } from '../../../lib/axios';
 
 interface CreateActivityModalProps {
   toggleCreateActivityModal: () => void;
 }
 
 export function CreateActivityModal({ toggleCreateActivityModal }: CreateActivityModalProps) {
+  const { tripId } = useParams();
+
+  function createActivity(event: React.FormEvent<HTMLFormElement>) {
+    event.preventDefault();
+
+    const date = new FormData(event.currentTarget);
+    const title = date.get('title')?.toString();
+    const occursAt = date.get('occurs_at')?.toString();
+
+    api.post(`/trips/${tripId}/activities`, { title, occurs_at: occursAt });
+
+    toggleCreateActivityModal();
+  }
+
   return (
     <div className="fixed inset-0 bg-black/60 flex items-center justify-center">
       <div className="w-[640px] rounded-xl py-5 px-6 shadow-shape bg-zinc-900 space-y-5">
@@ -23,8 +39,8 @@ export function CreateActivityModal({ toggleCreateActivityModal }: CreateActivit
         </div>
 
         {/* Add Activity Form */}
-        <form className="space-y-3">
-          <Input type="text" name="name" placeholder="Qual a atividade?">
+        <form onSubmit={createActivity} className="space-y-3">
+          <Input type="text" name="title" placeholder="Qual a atividade?">
             <Tag className="size-5 text-zinc-400" />
           </Input>
 
